dbcc73a4a6d2cc4a164b7b47a0202e08217a0aef
Jenny Zhu <jenny.a.zhu@gmail.com>
2023-03-24 09:38:54 -0400

    OU-151 Spike, Perses evalution Creation of a Perses dashboard with metrics pulled from an in-cluster Prometheus.

---
+++ web/src/components/DataSourceAPI.tsx
@@ -0,0 +1,50 @@
+import { GlobalDatasource } from '@perses-dev/core';
+import { DatasourceStoreProviderProps } from '@perses-dev/dashboards';
+
+export function useDatasourceApi(): DatasourceStoreProviderProps['datasourceApi'] {
+  return {
+    getDatasource: async (/*project, selector*/) => {
+      return undefined;
+    },
+    getGlobalDatasource: async (selector) => {
+      if (selector.kind === 'PrometheusDatasource' && selector.name === undefined) {
+        return {
+          resource: datasource,
+          proxyUrl: undefined,
+        };
+      }
+      return undefined;
+    },
+    listDatasources: async (/*project, pluginKind*/) => {
+      return [];
+    },
+    listGlobalDatasources: async (pluginKind) => {
+      if (pluginKind === datasource.spec.plugin.kind) {
+        return [datasource];
+      }
+      return [];
+    },
+  };
+}
+
+const datasource: GlobalDatasource = {
+  kind: 'GlobalDatasource',
+  metadata: {
+    name: 'PrometheusDemo',
+    created_at: '',
+    updated_at: '',
+    version: 0,
+  },
+  spec: {
+    default: true,
+    display: {
+      name: 'Prometheus Demo',
+    },
+    plugin: {
+      kind: 'PrometheusDatasource',
+      spec: {
+        direct_url: "http://localhost:9000/api/prometheus",
+      },
+    },
+  },
+};
--- web/src/components/PersesPage.tsx
+++ web/src/components/PersesPage.tsx
@@ -1,9 +1,70 @@
-import * as React from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryParamProvider } from "use-query-params";
+import { WindowHistoryAdapter } from "use-query-params/adapters/window";
+import { dashboardSample } from './SampleData';
+import { useDatasourceApi } from './DataSourceAPI';
+import { ViewDashboard } from '@perses-dev/dashboards';
+import { PersesDashboardProviders } from './PersesDashboardProvider';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
+
+const queryClient = new QueryClient({
+    defaultOptions: { 
+        queries: { 
+            refetchOnWindowFocus: false 
+        } 
+    }
+  });
+
+const datasourceApi = useDatasourceApi();
+
+// This MUI theme overrides Perses MUI styling. 
+const theme = createTheme({
+    components: {
+      // Name of the component
+      MuiButton: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            fontSize: '10px',
+            size: 'small'
+          },
+          outlined: {
+            color: 'black', 
+            borderColor: 'black',
+          }
+        },
+      },
+      MuiTypography: {
+        styleOverrides: {
+            h2: {
+                fontSize: '24px',
+                fontFamily: 'sans-serif',
+                fontWeight: '400',
+                justifyContent: 'space-between'
+            },
+        },
+      }
+    },
+  });
 
 export default function PersesPage(){
     return (
-        <div>
-            <h1> hello world </h1>
-        </div>
+        <ThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <QueryParamProvider adapter={WindowHistoryAdapter}>
+                    <Box>
+                        <PersesDashboardProviders>
+                            <ViewDashboard
+                            dashboardResource={dashboardSample}
+                            datasourceApi={datasourceApi}
+                            initialVariableIsSticky={false}
+                            isReadonly={true}
+                            />
+                        </PersesDashboardProviders>
+                    </Box>
+                </QueryParamProvider>
+            </QueryClientProvider>
+        </ThemeProvider>
     )
 }
